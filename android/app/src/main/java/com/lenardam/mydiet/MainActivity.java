5d196f9f7c23111f48e68e6a648a47ba1739c3bd
Damian Lenard <damian.lenard@gmail.com>
2024-12-10 20:30:53 +0100

    Dodanie nowych okien i przycisków

--- android/app/src/main/java/com/lenardam/mydiet/MainActivity.java
+++ android/app/src/main/java/com/lenardam/mydiet/MainActivity.java
@@ -1,6 +1,9 @@
 package com.lenardam.mydiet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button recipes_button;
+    private Button shopping_list_button;
+    private Button settings_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recipes_button = (Button) findViewById(R.id.recipes_button);
+        shopping_list_button = (Button) findViewById(R.id.shopping_list_button);
+        settings_button = (Button) findViewById(R.id.settings_button);
+
+        recipes_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RecipesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        shopping_list_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ShoppingListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        settings_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
