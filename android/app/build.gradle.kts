5f5315134a5b1271e4ca6d79ab8f4488daa0e0a6
Damian Lenard <damian.lenard@gmail.com>
2025-09-19 09:58:16 +0200

    nowy versionCode - test zamknięty

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lenardam.mydiet"
         minSdk = 31
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.01"
+        versionCode = 4
+        versionName = "1.02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
