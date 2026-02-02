94ffb8ba8b83d9bbd9cc6274aa2cd1b17a374e46
Damian Lenard <damian.lenard@gmail.com>
2026-02-02 12:19:55 +0100

    SDK 29

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.lenardam.mydiet"
-        minSdk = 31
+        minSdk = 29
         targetSdk = 35
         versionCode = 10
         versionName = "1.07"
